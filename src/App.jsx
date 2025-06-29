c8ba4dffb3e975473654e217429c779334bbdc65
sujal kumar <sujalgupta7601@gmail.com>
2025-06-29 23:58:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Content from "./components/Content";
+
+const App=()=>{
+   return(
+    <>
+    <Header/>
+    <NavBar/>
+    <Content/>
+    </>
+   )
+}
+export default App
